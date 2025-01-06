f9c837152060d1fa973dc6968e6474a114c79580
Suketu7879 <suketurola4@gmail.com>
2025-01-06 14:14:04 +0530

    new quiz commit

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,7 @@ import { questions } from "../set/quiz.js";
 
 export const getQuestion = async (req, res) => {
   try {
-    const random = Math.ceil(Math.random() * 200);
+    const random = Math.ceil(Math.random() * 400);
     const result = questions[random];
     if (!result) {
       return res.status(400).json({ message: "something went wrong" });
